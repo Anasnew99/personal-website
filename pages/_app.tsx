4ce35c077aa2804462dca45f650fd031d51c7765
Anas <anasnew99@gmail.com>
2022-05-03 00:06:41 +0530

    Initial Setup, MUI, Emotion, Motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import createEmotionCache from "../utils/emotionCache";
+import theme from "../styles/theme";
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+const clientSideEmotionCache = createEmotionCache();
+type ExtendedAppProps = AppProps & {
+  emotionCache: typeof clientSideEmotionCache;
+};
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: ExtendedAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
